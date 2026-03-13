d4a5332569c1f8fcfad284890d1dc327064df51c
leonardo-monti <monti.leonardo22@gmail.com>
2026-03-13 16:24:29 +0100

    Milestone 2

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ app.get ("/",(req,res)=>{
     res.send ("Hello World")
 })
 
+app.get ("/posts",(req,res)=>{
+    const sql= "SELECT * FROM posts"
+    connection.query(sql, (err,results)=>{
+        if(err) {
+            return res.status(500).json({
+                success:false,
+                message: "Database query failed"
+            })
+        }
+        res.json({
+            success:true,
+            message:"Post del database",
+            result:results
+        })
+    })
+})
+
 app.listen(port,()=>{
     console.log("Server listening on http://localhost:3000/")
 })
