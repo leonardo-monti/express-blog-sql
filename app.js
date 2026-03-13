e6568d96d27e3d2b368bde20a618afd209cb9a1a
leonardo-monti <monti.leonardo22@gmail.com>
2026-03-13 16:03:49 +0100

    Milestone 1

---
+++ app.js
@@ -0,0 +1,13 @@
+const express =require ('express')
+const app= express()
+const port = 3000
+
+const connection= require ("./db/connection")
+
+app.get ("/",(req,res)=>{
+    res.send ("Hello World")
+})
+
+app.listen(port,()=>{
+    console.log("Server listening on http://localhost:3000/")
+})
